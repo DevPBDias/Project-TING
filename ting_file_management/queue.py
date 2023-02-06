db385d57bf0b8ca6c33657d334ac990559ebe5d7
DevPBDias <devpbdias@gmail.com>
2023-02-06 16:18:05 -0300

    fix: creating bussiness rules to class Queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,27 +1,26 @@
 from ting_file_management.abstract_queue import AbstractQueue
-
+# python3 -m pytest tests/test_queue.py
 
 class Queue(AbstractQueue):
     FIRST_ELEMENT = 0
 
     def __init__(self):
-        self._data = []
+        self._data = [] # criar uma lista ou list()
 
-    def __len__(self):
+    def __len__(self): # tamanho da fila
         return len(self._data)
 
-    def enqueue(self, value):
+    def enqueue(self, value): # acrescenta no final da fila
         self._data.append(value)
 
     def dequeue(self):
-        if self._data:
-            return self._data.pop(self.FIRST_ELEMENT)   
-        return None
+        try:
+            if len(self._data) != 0: # se o len for maior q 0, entao retira o primeiro elemento
+                return self._data.pop(self.FIRST_ELEMENT)   
+        except:
+            return None
 
     def search(self, index):
-        try:
-            if self._data:
-                return self._data[self.index]   
-            return None
-        except IndexError:
-            raise IndexError(f"Índice inválido") 
+        if 0 <= index < len(self._data):
+            return self._data[index]   
+        raise IndexError
